2f559b21ab80a3d599e2958e8fbfc7621708ec4f
Santiago <ssouto@dvelop.com.uy>
2021-11-15 16:52:44 -0300

    commit - deploy & parameters

--- Canastas/EnvLocal/parameters.groovy
+++ Canastas/EnvLocal/parameters.groovy
@@ -1,5 +1,19 @@
-def credentials = CredentialsProvider.lookupCredentials(StandardUsernamePasswordCredentials.class, Jenkins.instance);
-def cred = credentials.findResult { it.id == "credentials-genexusserver17" ? it : null }
+import jenkins.*
+import jenkins.model.* 
+import hudson.*
+import hudson.model.*
+def jenkinsCredentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+        com.cloudbees.plugins.credentials.Credentials.class,
+        Jenkins.instance,
+        null,
+        null
+);
+for (creds in jenkinsCredentials) {
+    if (jenkinsCredentials.id == "credentials-genexusserver17") {
+        def cred = creds
+        break
+    }
+}
 
 def getForEnvironment(envName) {
 
