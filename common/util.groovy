8f93f982ccb3fc348d4f9aa1f404a0208597b720
Santiago <ssouto@dvelop.com.uy>
2021-11-22 14:40:38 -0300

    commit - util

--- common/util.groovy
+++ common/util.groovy
@@ -32,7 +32,7 @@ def cloneRepo(env, gitEnv, name) {
 
 def gitWebApp(env, webapp, comment) {
 
-    "git checkout ${env} git add ${webapp} git commit -am '${comment} git push"
+    "git checkout ${env} git add ${webapp} git commit -am '${comment}' git push"
     // "git add ${webapp}"
     // "git commit -am '${comment}'"    
     // "git push"
