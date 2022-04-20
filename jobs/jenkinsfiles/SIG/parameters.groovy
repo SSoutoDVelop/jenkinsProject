97aedd36f39466894e3d6f80e18ee7a10c0564cb
Santiago <ssouto@dvelop.com.uy>
2022-04-20 14:42:48 -0300

    commit - minnor changes & SIG testing

---
+++ jobs/jenkinsfiles/SIG/parameters.groovy
@@ -0,0 +1,273 @@
+def getForEnvironment(envName, envVersion) {
+
+    // Configuration root object
+    def config              = [:]
+    config.General          = [:]
+    config.Git              = [:]
+    config.GXServer         = [:]    
+    config.Environment      = [:]
+    config.Datastores       = [:]
+    config.DeploymentUnits  = [:]
+    config.WebGenerator     = [:]
+
+    config.General.Name                     = "sig"
+    config.General.WebAppName               = "sig"
+    config.General.VersionSuffix            = "_v${env.BUILD_NUMBER}_${envName}"
+    config.General.GXProgramDirKey          = "gx-programdir-17U7"
+    config.General.DeploymentPlatform       = "OKD"
+
+    config.WebGenerator.LogLevelGX = "ERROR"
+    config.WebGenerator.LogLevelUser = "WARN"
+    config.WebGenerator.LogOutput = "File"
+    config.WebGenerator.LogFile = "/app/logs/client.log"    
+    
+    config.General.ForceRebuild         = "False"  
+
+    //================================//
+    // DEPLOYMENT UNITS CONFIGURATION
+    //================================//
+
+    config.DeploymentUnits["DUWeb"]                              = [:]
+    config.DeploymentUnits["DUWeb"].DeployTargetId               = "LOCAL" 
+    config.DeploymentUnits["DUWeb"].DeployApplicationCompiler    = "JAVA" 
+    config.DeploymentUnits["DUWeb"].DeploySelectedObjectsOnly    = "False"
+    config.DeploymentUnits["DUWeb"].DeployApplicationServer      = "Tomcat 8.x"
+    config.DeploymentUnits["DUWeb"].DeployUseAppServerDatasource = "False" 
+    config.DeploymentUnits["DUWeb"].DeployIncludeGAM             = "False" 
+    config.DeploymentUnits["DUWeb"].DeployGXflowbackoffice       = "True" 
+    config.DeploymentUnits["DUWeb"].DeployTargetJRE              = "9" 
+    config.DeploymentUnits["DUWeb"].DeployPackageFormat          = "Automatic" 
+    config.DeploymentUnits["DUWeb"].DeployTimeStamp              = "${env.BUILD_TIMESTAMP}"
+    config.DeploymentUnits["DUWeb"].DeployFileFullPath           = "${env.WORKSPACE}" 
+    config.DeploymentUnits["DUWeb"].DeployWebappName             = config.General.Name
+    config.DeploymentUnits["DUWeb"].DeployProjectName            = "${config.General.WebAppName}${config.General.VersionSuffix}" 
+    config.DeploymentUnits["DUWeb"].DeployFullPath               = "${env.WORKSPACE}\\${config.General.WebAppName}${config.General.VersionSuffix}" 
+    config.DeploymentUnits["DUWeb"].DeployObjectNames            = "DeploymentUnit:DUWeb"      
+    config.DeploymentUnits["DUWeb"].VersionEnvironment           = "web"   
+
+    config.DeploymentUnits["DUMobile"]                              = [:]
+    config.DeploymentUnits["DUMobile"].DeployTargetId               = "LOCAL" 
+    config.DeploymentUnits["DUMobile"].DeployApplicationCompiler    = "JAVA" 
+    config.DeploymentUnits["DUMobile"].DeploySelectedObjectsOnly    = "False"
+    config.DeploymentUnits["DUMobile"].DeployApplicationServer      = "Tomcat 8.x"
+    config.DeploymentUnits["DUMobile"].DeployUseAppServerDatasource = "False" 
+    config.DeploymentUnits["DUMobile"].DeployIncludeGAM             = "False" 
+    config.DeploymentUnits["DUMobile"].DeployGXflowbackoffice       = "True" 
+    config.DeploymentUnits["DUMobile"].DeployTargetJRE              = "9" 
+    config.DeploymentUnits["DUMobile"].DeployPackageFormat          = "Automatic" 
+    config.DeploymentUnits["DUMobile"].DeployTimeStamp              = "${env.BUILD_TIMESTAMP}"    
+    config.DeploymentUnits["DUMobile"].DeployFileFullPath           = "${env.WORKSPACE}" 
+    config.DeploymentUnits["DUMobile"].DeployWebappName             = config.General.Name
+    config.DeploymentUnits["DUMobile"].DeployProjectName            = "${config.General.WebAppName}${config.General.VersionSuffix}" 
+    config.DeploymentUnits["DUMobile"].DeployFullPath               = "${env.WORKSPACE}\\${config.General.WebAppName}${config.General.VersionSuffix}" 
+    config.DeploymentUnits["DUMobile"].DeployObjectNames            = "DeploymentUnit:DUMobile"     
+    config.DeploymentUnits["DUMobile"].VersionEnvironment           = "mobile"     
+
+    // Android Settings
+    config.SDGenerator.SmartDevicesAndroidSDK               = "C:\\Android-SDK"
+    config.SDGenerator.SmartDevicesAndroidKeystoreFile      = "C:\\DVelop\\dvelop-montes.keystore"
+    config.SDGenerator.SmartDevicesAndroidKeyAlias          = "dvelop-montes"
+    config.SDGenerator.SmartDevicesAndroidKeyStorePassword  = "montes"
+    config.SDGenerator.SmartDevicesAndroidKeyPassword       = "montes"
+    config.SDGenerator.SmartDevicesAndroidCompilationMode   = "COMPILATION_MODE_DISTRIBUTION" 
+
+    if(envName.equals("EnvLocal")) {
+
+        config.General.WorkingEnvironment       = "EnvLocal"    
+        config.General.DeployEnvironment        = "EnvLocal"  
+        config.General.WorkingVersion           = "SIG" 
+        config.General.GXWorkingDirectoryKey    = "gx-sig_local-workingdir"
+
+        //=======================//
+        // GENERAL CONFIGURATION
+        //=======================//     
+
+        config.Datastores["Default"] = [:]
+        config.Datastores["Default"].DatastoreServer = "localhost"
+        config.Datastores["Default"].DatastoreDatabase = "SIGLocal"
+        config.Datastores["Default"].DatastoreUseJDBCCustomUrl = "True"
+        config.Datastores["Default"].DatastoreJDBCCustomUrl = "jdbc:jtds:sqlserver://localhost:1433/SIGLocal"            
+
+        config.Datastores["GAM"] = [:]
+        config.Datastores["GAM"].DatastoreServer = "localhost"
+        config.Datastores["GAM"].DatastoreDatabase = "SIGGAM"
+        config.Datastores["GAM"].DatastoreUseJDBCCustomUrl = "True"
+        config.Datastores["GAM"].DatastoreJDBCCustomUrl = "jdbc:jtds:sqlserver://localhost:1433/SIGGAM"            
+
+        config.Environment.GAMRepositoryId  = "41474542-c051-4909-b51c-607730d0da8d" 
+
+        config.Datastores["MdPStaging"] = [:]
+        config.Datastores["MdPStaging"].DatastoreServer = "localhost"
+        config.Datastores["MdPStaging"].DatastoreDatabase = "MdP_Staging"
+        config.Datastores["MdPStaging"].DatastoreUseJDBCCustomUrl = "True"
+        config.Datastores["MdPStaging"].DatastoreJDBCCustomUrl = "jdbc:jtds:sqlserver://localhost:1433/MdP_Staging"   
+
+        config.Datastores["Portal"] = [:]
+        config.Datastores["Portal"].DatastoreServer = "localhost"
+        config.Datastores["Portal"].DatastoreDatabase = "PortalLocal"
+        config.Datastores["Portal"].DatastoreUseJDBCCustomUrl = "True"
+        config.Datastores["Portal"].DatastoreJDBCCustomUrl = "jdbc:jtds:sqlserver://localhost:1433/PortalLocal"          
+
+        //================================//
+        // GIT CONFIGURATION
+        //================================//    
+
+        config.Git.GitEnvironment = "dev" //GitHub Testing           
+
+    }
+
+    if(envName.equals("EnvStable")) {
+
+        config.General.WorkingEnvironment       = "EnvRelease"   
+        config.General.DeployEnvironment        = "EnvStable"  
+        config.General.WorkingVersion           = "SIG" 
+        config.General.GXWorkingDirectoryKey    = "gx-sig_stable-workingdir" 
+
+        //=======================//
+        // GENERAL CONFIGURATION
+        //=======================//        
+        config.Datastores["Default"] = [:]
+        config.Datastores["Default"].DatastoreServer = "uycls267.mdp.local"
+        config.Datastores["Default"].DatastoreDatabase = "SIG_Dev"
+        config.Datastores["Default"].DatastoreUseJDBCCustomUrl = "False"
+        config.Datastores["Default"].CredentialId = "BDServer_uycls267"
+
+        config.Datastores["GAM"] = [:]
+        config.Datastores["GAM"].DatastoreServer = "uycls267.mdp.local"
+        config.Datastores["GAM"].DatastoreDatabase = "Portal_Dev"
+        config.Datastores["GAM"].DatastoreUseJDBCCustomUrl = "False" 
+        config.Datastores["GAM"].CredentialId = "BDServer_uycls267"          
+
+        config.Environment.GAMRepositoryId  = "41474542-c051-4909-b51c-607730d0da8d" 
+
+        config.Datastores["MdPStaging"] = [:]
+        config.Datastores["MdPStaging"].DatastoreServer = "uycls205.mdp.local"
+        config.Datastores["MdPStaging"].DatastoreDatabase = "MdP_Staging"
+        config.Datastores["MdPStaging"].DatastoreUseJDBCCustomUrl = "False"  
+        config.Datastores["MdPStaging"].CredentialId = "BDServer_uycls205"
+
+        config.Datastores["Portal"] = [:]
+        config.Datastores["Portal"].DatastoreServer = "uycls267.mdp.local"
+        config.Datastores["Portal"].DatastoreDatabase = "Portal_Dev"
+        config.Datastores["Portal"].DatastoreUseJDBCCustomUrl = "False"  
+        config.Datastores["Portal"].CredentialId = "BDServer_uycls267"    
+
+        config.Git.GitEnvironment = envVersion.equals("mobile") == true ? "dev-${envVersion}" : "dev" //GitHub MdP    
+
+        // Services URL
+        config.SDGenerator.SmartDevicesServicesURL  = "http://sigdev.mdp.local/"         
+
+    }
+
+    if(envName.equals("EnvRelease")) {
+
+        config.General.WorkingEnvironment       = "EnvRelease"  
+        config.General.DeployEnvironment        = "EnvRelease"  
+        config.General.WorkingVersion           = "SIG"  
+        config.General.GXWorkingDirectoryKey    = "gx-sig_release-workingdir" 
+
+        //=======================//
+        // GENERAL CONFIGURATION
+        //=======================//        
+        config.Datastores["Default"] = [:]
+        config.Datastores["Default"].DatastoreServer = "localhost"
+        config.Datastores["Default"].DatastoreDatabase = "SIGRelease"
+        config.Datastores["Default"].DatastoreUseJDBCCustomUrl = "False"
+        config.Datastores["Default"].CredentialId = "BDServer_localhost"
+
+        config.Datastores["GAM"] = [:]
+        config.Datastores["GAM"].DatastoreServer = "uycls267.mdp.local"
+        config.Datastores["GAM"].DatastoreDatabase = "Portal_Qa"
+        config.Datastores["GAM"].DatastoreUseJDBCCustomUrl = "False"  
+        config.Datastores["GAM"].CredentialId = "BDServer_uycls267"         
+
+        config.Environment.GAMRepositoryId  = "41474542-c051-4909-b51c-607730d0da8d" 
+
+        config.Datastores["MdPStaging"] = [:]
+        config.Datastores["MdPStaging"].DatastoreServer = "uycls205.mdp.local"
+        config.Datastores["MdPStaging"].DatastoreDatabase = "MdP_Staging"
+        config.Datastores["MdPStaging"].DatastoreUseJDBCCustomUrl = "False" 
+        config.Datastores["MdPStaging"].CredentialId = "BDServer_uycls205"  
+
+        config.Datastores["Portal"] = [:]
+        config.Datastores["Portal"].DatastoreServer = "uycls267.mdp.local"
+        config.Datastores["Portal"].DatastoreDatabase = "Portal_Qa"
+        config.Datastores["Portal"].DatastoreUseJDBCCustomUrl = "False" 
+        config.Datastores["Portal"].CredentialId = "BDServer_uycls267"         
+
+        config.Git.GitEnvironment = envVersion.equals("mobile") == true ? "qa-${envVersion}" : "qa" //GitHub MdP
+
+        // Services URL
+        config.SDGenerator.SmartDevicesServicesURL  = "https://sigqa.montesdelplata.com.uy/"   
+    }
+
+    withCredentials([
+        string(
+            credentialsId: config.General.GXProgramDirKey, 
+            variable: 'programdir'),
+        string(
+            credentialsId: config.General.GXWorkingDirectoryKey, 
+            variable: 'workingdir')
+        ]) {    
+        config.General.GX_PROGRAM_DIR   = "${programdir}"
+        config.General.WorkingDirectory = "${workingdir}"
+    }    
+
+    //=========================================//
+    // PATHING CONFIGURATION
+    //=========================================//    
+
+    config.General.EnvironmentRelativePath  = "${config.General.WorkingEnvironment}"
+    config.General.EnvironmentRootFolder    = "${config.General.WorkingDirectory}\\${config.General.EnvironmentRelativePath}"
+    config.General.EnvironmentWebFolder     = "${config.General.EnvironmentRootFolder}\\web"
+    config.General.EnvironmentMobileFolder  = "${config.General.EnvironmentRootFolder}\\mobile"
+
+    withCredentials([
+        string(
+            credentialsId: 'git-path', 
+            variable: 'path')
+        ]) {    
+        config.Git.GitFolder    = "${path}"
+        config.Git.GitEnvFolder = "${path}${config.General.Name}_${config.Git.GitEnvironment}"
+    }        
+
+    //=========================================//
+    // GENEXUS SERVER CONNECTION CONFIGURATION
+    //=========================================//
+
+    config.GXServer.GXServerUrl                 = "http://gxserver.montesdelplata.com.uy/genexusserver17"
+    config.GXServer.GXServerCredentialsKey      = "credentials-genexusserver17"
+    // config.GXServer.GXServerUsername            = ""
+    // config.GXServer.GXServerPassword            = ""
+    config.GXServer.GXServerKB                  = config.General.WorkingVersion
+    config.GXServer.GXServerVersion             = config.General.WorkingVersion 
+    config.GXServer.GXServerChangelog           = "${env.WORKSPACE}\\Changelog${config.General.VersionSuffix}.xml" 
+    config.GXServer.GXServerFreezeAfterDeploy   = "False"     
+
+    // GAM connection information
+    config.Environment.GAMAdminCredentialKey        = "credentials-gamadmin"
+    // config.Environment.GAMAdminUsername             = ""
+    // config.Environment.GAMAdminPassword             = ""
+    config.Environment.GAMConnectionCredentialKey   = "credentials-sig-gamconnection"
+    // config.Environment.GAMConnectionUsername        = ""
+    // config.Environment.GAMConnectionPassword        = ""
+
+    // Use parameter encryption in URLs
+    config.Environment.UseEncryption = "NO"
+
+    // Use HTTP or HTTPS protocol
+    config.Environment.HttpProtocol = "Secure"
+
+    // Set Environment Path in disk
+    config.Environment.TargetPath = "${config.General.EnvironmentRelativePath}"  
+
+    // Esta reorganización simplemente mantiene a GAM actualizado, pero nunca va crear la tabla de GAM siempre y cuando esta exista. (Creación de permisos, contemplación de cambio de versión sobre migraciones, entre otros)     
+    config.Environment.ReorganizeGamDatabase    = "True" 
+
+    // Con este parámetro evitamos que se realice una reoganización de las tablas correspondientes al sistema en si mismo   
+    config.Environment.DoNotExecuteReorg        = "True"       
+
+    return config
+}
+
+return this
--- jobs/jenkinsfiles/common/util.groovy
+++ jobs/jenkinsfiles/common/util.groovy
@@ -1,7 +1,9 @@
 def getMSBuildParams(dictionary) {
     def params = ""
     for (p in dictionary) {
-        params += "/p:${p.key}=\"${p.value}\" "
+        if (p.key != "CredentialId") {
+            params += "/p:${p.key}=\"${p.value}\" "
+        }
     }
     return params
 }
@@ -20,13 +22,13 @@ def cloneRepo(env, gitEnv, name) {
 
         case "EnvLocal":
 
-            return "git clone -b ${gitEnv} https://github.com/SSoutoDVelop/canastas.git ${name}_${gitEnv}"
+            return "git clone -b ${gitEnv} https://github.com/SSoutoDVelop/${name}.git ${name}_${gitEnv}"
 
             break
 
         case "EnvStable" || "EnvRelease":
 
-            return "git clone -b ${gitEnv} https://soporte.dvelop:MDPOctubre.2021@gitlab.montesdelplata.com.uy/OKD/canastas.git ${name}_${gitEnv}"
+            return "git clone -b ${gitEnv} https://soporte.dvelop:MDPOctubre.2021@gitlab.montesdelplata.com.uy/OKD/${name}.git ${name}_${gitEnv}"
 
             break
 
@@ -37,6 +39,7 @@ def cloneRepo(env, gitEnv, name) {
 def gitCheckout(env) {
 
     return "git checkout ${env}"
+
 }
 
 def gitAdd(webapp) {
@@ -51,9 +54,27 @@ def gitCommit(comment) {
 
 }
 
+def gitPull() {
+
+    return "git pull"
+
+}
+
 def gitPush() {
 
     return "git push"
+
+}
+
+def deployAzure(user, pass, zipfile, apiurl) {
+
+    return "curl -X PUT -u ${user}:${pass} --data-binary @${zipfile} ${apiurl}"
+
+}
+
+def deployTareasProgramadasAzure(zipfile, taskDir) {
+
+    return '"C:\\Program Files (x86)\\IIS\\Microsoft Web Deploy V3\\msdeploy"' + " -verb:sync -skip:Directory=Logs -source:package=${zipfile} -dest:contentPath=C:\\DVelop\\TareasProgramadas\\${taskDir},computerName=clazs003"
 
 }
 
